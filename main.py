7ebfb1e4272027e1965fe5e9738ec26a37d6c954
dmdar112 <ahshag1727@gmail.com>
2025-06-28 21:50:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -494,14 +494,14 @@ def start(message):
             reply_markup=owner_inline_keyboard()
         )
         bot.send_message(user_id, "✅ تم تحديث لوحة التحكم.", reply_markup=types.ReplyKeyboardRemove())
-    elif has_v1_access or has_v2_access: # User is activated (has access to either category)
+        elif has_v1_access or has_v2_access: # User is activated (has access to either category)
         if requires_mandatory_check and not is_currently_subscribed_to_all_mandatory_channels(user_id):
-    # If check is enabled and user is not subscribed to all mandatory channels
-    send_mandatory_subscription_message(user_id)
-else:
-    # User is activated and subscribed to all mandatory channels (or check is disabled)
-    welcome_message = "📂اختر قسم الفيديوهات من الأزرار بالأسفل!"
-    bot.send_message(user_id, welcome_message, reply_markup=main_keyboard())
+            # If check is enabled and user is not subscribed to all mandatory channels
+            send_mandatory_subscription_message(user_id)
+        else:
+            # User is activated and subscribed to all mandatory channels (or check is disabled)
+            # تم تعديل رسالة الترحيب لتكون كما طلبت
+            bot.send_message(user_id, "📂اختر قسم الفيديوهات من الأزرار بالأسفل!", reply_markup=main_keyboard())
     else: # User is not activated at all
         markup_for_unactivated = initial_activation_keyboard()
         activation_message_text = (
